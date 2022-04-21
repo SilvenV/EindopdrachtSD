42dd1c058b81dac0b8204bc14795e23c7acfb2d4
SilvenV <64805978+SilvenV@users.noreply.github.com>
2022-04-21 11:00:35 +0200

    Add files via upload
    First commit, test.

---
+++ EindopdrachtSD/src/main/java/com/example/eindopdrachtsd/EindopdrachtSdApplication.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtSdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtSdApplication.class, args);
+    }
+
+}
---
+++ EindopdrachtSD/src/test/java/com/example/eindopdrachtsd/EindopdrachtSdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtSdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
